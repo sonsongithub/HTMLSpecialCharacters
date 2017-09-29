702d21a1bc27e93e201fba942c68eef4db61a13d
sonson <yoshida.yuichi@gmail.com>
2017-09-30 01:09:50 +0900

    Trying to support macOS.

---
+++ HTMLSpecialCharacters/HTMLSpecialCharacters.h
@@ -0,0 +1,18 @@
+//
+//  HTMLSpecialCharacters.h
+//  HTMLSpecialCharacters
+//
+//  Created by sonson on 2017/09/30.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for HTMLSpecialCharacters.
+FOUNDATION_EXPORT double HTMLSpecialCharactersVersionNumber;
+
+//! Project version string for HTMLSpecialCharacters.
+FOUNDATION_EXPORT const unsigned char HTMLSpecialCharactersVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HTMLSpecialCharacters/PublicHeader.h>
+
+
